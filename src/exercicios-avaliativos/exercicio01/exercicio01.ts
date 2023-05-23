625625353f6429f19e86fa04adeb830736c4a722
Josué Lustosa <josuelustosa.job@gmail.com>
2023-05-23 19:39:24 -0400

    feat: exercicio 01

---
+++ src/exercicios-avaliativos/exercicio01/exercicio01.ts
@@ -0,0 +1,29 @@
+let livros: any[][];
+livros = [
+  ["Resiliência – A trajetória de Dong Yu Lan", 107.9, "Liderança"],
+  ["Fundamentos da fé cristã – volume 1", 44.0, "Vida Cristã"],
+  ["Fundamentos da fé cristã – volume 2", 44.0, "Vida Cristã"],
+  ["Fundamentos da fé cristã – volume 3", 44.0, "Vida Cristã"],
+  ["Fundamentos da fé cristã – volume 4", 44.0, "Vida Cristã"],
+  ["Filhos… Como conduzi-los a Deus", 20.3, "Família"],
+  ["Casamento: Um jardim de sonhos? ou um lugar sem cor?", 48.2, "Família"],
+  [
+    "Mulheres da Bíblia: suas histórias e legados (Antigo Testamento)",
+    30.0,
+    "Mulher",
+  ],
+  [
+    "Mulheres da Bíblia: suas histórias e legados (Novo Testamento)",
+    39.9,
+    "Mulher",
+  ],
+  ["Uma mulher virtuosa em construção", 29.9, "Mulher"],
+  ["Uma mulher corajosa em construção", 29.9, "Mulher"],
+  ["A nossa esperança não se limita a esta vida", 31.1, "Jovens"],
+  ["Atitude: Um jovem vencedor!", 20.3, "Jovens"],
+  ["O nome acima de todo nome", 19.6, "Jovens"],
+  ["#partiu", 13.6, "Jovens"],
+  ["Amar a vinda do Senhor", 25.0, "Escatalogia"],
+];
+
+console.table(livros);
